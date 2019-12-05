891714db7aa859b8ef38b9f753e319a0be74efae
Arnas Kazlauskas <arnkzl@telesoftas.com>
2019-12-05 13:36:46 +0200

    initial

---
+++ tests/MainTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Test;
+
+use Main;
+
+use Symfony\Bundle\FrameworkBundle\Tests\Testcase;
+
+class MainTest extends TestCase
+{
+
+}
---
+++ tests/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Test;
+
+use App\User;
+
+use Symfony\Bundle\FrameworkBundle\Tests\Testcase;
+
+class UserTest extends TestCase
+{
+
+}
